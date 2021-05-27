75e70a2eb1554c65cd350dc621c343e77bdc107c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-27 22:48:59 +0900

    :bookmark_tabs: :: [Config] Typeorm-Config 추가

---
+++ src/config/ormconfig.ts
@@ -0,0 +1,14 @@
+import config from "./index";
+import { ConnectionOptions } from "typeorm";
+
+export const connectionOption: ConnectionOptions = {
+  type: "mysql",
+  host: config.mysql.dbHost,
+  port: Number(config.mysql.dbPort),
+  username: config.mysql.dbUser,
+  password: config.mysql.dbPass,
+  database: config.mysql.dbName,
+  synchronize: false,
+  logging: false,
+  entities: []
+}
