832dd73921c6721f97a331b7982aef331938ae56
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-28 10:26:36 +0900

    🧪 :: [Test] getPostCatalog 테스트코드 추가

--- src/test/service/post.spec.ts
+++ src/test/service/post.spec.ts
@@ -1,6 +1,16 @@
+import { Post } from "../../interface/post/post";
+import { PostService } from "../../service/post";
+import { MockPostRepository } from "../repository/MockPostRepository";
+
+const postService: PostService = new PostService(MockPostRepository.instance);
+
   describe("PostService", () => {
-    
     describe("getPostCatalog", () => {
-      
+      it("should return post catalog", () => {
+        postService.getPostCatalog()
+        .then((res: Array<Post>) => {
+          expect(res).toBeInstanceOf(Array);
+        });
+      })
     });
   });
