cab9ed9dad86701f7ff396f8b0f2756a7738a1ab
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-30 18:19:23 +0900

    ⚡ :: [Exception] uploadPost 예외

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -4,3 +4,4 @@ export const forbiddenUserException = new HttpException(403, "Forbidden User");
 export const notFoundUserException = new HttpException(400, "Not Found User");
 export const invalidPasswordException = new HttpException(400, "Invalid Password");
 export const alreadyExistUserException = new HttpException(400, "Already Exist User");
+export const internalServerError = new HttpException(500, "Internal Server Error");
