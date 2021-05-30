0a1438115a7c5684111aca9127f9432b9c29e697
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-30 14:33:32 +0900

    🧪 :: [Test] loginUser 테스트코드 완성

--- src/test/service/user.spec.ts
+++ src/test/service/user.spec.ts
@@ -1,7 +1,20 @@
-import { forbiddenUserException } from "../../exception";
+import { LoginUserRequest, LoginUserResponse, TokenPayload } from "../../interface";
+import { forbiddenUserException, invalidPasswordException, notFoundUserException } from "../../exception";
 import { UserService } from "../../service/user";
 import { dummyUser } from "../dummy/user";
 import { MockUserRepository } from "../repository/MockUserRepository";
+
+jest.mock("bcrypt", () => ({
+  async compare(data: string, encrypted: string): Promise<boolean> {
+    return data === encrypted;
+  }
+}));
+
+jest.mock("jsonwebtoken", () => ({
+  async sign({ type }: TokenPayload) {
+    return type === "access" ? "accessToken" : "refreshToken";
+  }
+}));
 
 const userService: UserService = new UserService(MockUserRepository.instance);
 
@@ -20,6 +33,33 @@ describe("UserService", () => {
   describe("signUpUser", () => {
     it("should success", () => {
       return userService.signUpUser(dummyUser);
+    });
+  });
+  describe("loginUser", () => {
+    const body: LoginUserRequest = {
+      id: "rightId",
+      password: "asdf"
+    };
+    it("should throw notFound user exception", (done) => {
+      userService.loginUser({ ...body, id: "invalidId" })
+      .catch(err => {
+        expect(err).toEqual(notFoundUserException);
+        done();
+      });
+    });
+    it("should throw invalid password exception", (done) => {
+      userService.loginUser({ ...body, password: "invaildPassword" })
+      .catch(err => {
+        expect(err).toEqual(invalidPasswordException);
+        done();
+      });
+    });
+    it("should return accessToken and refreshToken", () => {
+      return userService.loginUser(body)
+      .then((res: LoginUserResponse) => {
+        expect(res.accessToken).toEqual("accessToken");
+        expect(res.refreshToken).toEqual("refreshToken");
+      });
     });
   });
 });
