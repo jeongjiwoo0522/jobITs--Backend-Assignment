a14aa415326c1a61f32546eed781d7576d0fe70d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-30 14:31:44 +0900

    ⚡ :: [Exception] 로그인 예외

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -1,3 +1,5 @@
 import { HttpException } from "./httpException";
 
 export const forbiddenUserException = new HttpException(403, "Forbidden User");
+export const notFoundUserException = new HttpException(400, "Not Found User");
+export const invalidPasswordException = new HttpException(400, "Invalid Password");
