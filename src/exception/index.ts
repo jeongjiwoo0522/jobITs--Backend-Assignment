963ec5a54d774d8cc8cf5feab538dac2c962e97d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-30 15:43:22 +0900

    ⚡ :: [Exception] 회원가입 예외

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -3,3 +3,4 @@ import { HttpException } from "./httpException";
 export const forbiddenUserException = new HttpException(403, "Forbidden User");
 export const notFoundUserException = new HttpException(400, "Not Found User");
 export const invalidPasswordException = new HttpException(400, "Invalid Password");
+export const alreadyExistUserException = new HttpException(400, "Already Exist User");
