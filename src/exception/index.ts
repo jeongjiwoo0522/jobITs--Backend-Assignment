eb57d69ada157bb0e1bad297e4203a5b54079949
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-30 21:37:58 +0900

    ⚡ :: [Exception] removePost 예외

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -4,4 +4,5 @@ export const forbiddenUserException = new HttpException(403, "Forbidden User");
 export const notFoundUserException = new HttpException(400, "Not Found User");
 export const invalidPasswordException = new HttpException(400, "Invalid Password");
 export const alreadyExistUserException = new HttpException(400, "Already Exist User");
+export const notFoundPostException = new HttpException(400, "Not Found Post");
 export const internalServerError = new HttpException(500, "Internal Server Error");
