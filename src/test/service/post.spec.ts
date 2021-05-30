7095191ad95af52056f7989035a00855ceb2eeca
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-30 18:41:54 +0900

    🧪 :: [Test] uploadPost 테스트코드 추가

--- src/test/service/post.spec.ts
+++ src/test/service/post.spec.ts
@@ -1,16 +1,60 @@
 import { Post } from "../../interface";
 import { PostService } from "../../service/post";
+import { MockImageRepository } from "../repository/MockImageRepository";
 import { MockPostRepository } from "../repository/MockPostRepository";
+import { MockUserRepository } from "../repository/MockUserRepository";
 
-const postService: PostService = new PostService(MockPostRepository.instance);
+const mockQueryRunner = {
+  connect: jest.fn(),
+  startTransaction: jest.fn(),
+  commitTransaction: jest.fn(),
+  rollbackTransaction: jest.fn(),
+  release: jest.fn()
+};
 
-  describe("PostService", () => {
-    describe("getPostCatalog", () => {
-      it("should return post catalog", () => {
-        postService.getPostCatalog()
-        .then((res: Array<Post>) => {
-          expect(res).toBeInstanceOf(Array);
-        });
-      })
+jest.mock("typeorm", () => ({
+  getConnection() {
+    return {
+      createQueryRunner() {
+        return mockQueryRunner;
+      }
+    }
+  }
+}));
+
+jest.mock("uuid", () => ({
+  v4() {
+    return "postId";
+  }
+}))
+
+const postService: PostService = new PostService(MockPostRepository.instance, MockImageRepository.instance, MockUserRepository.instance);
+
+describe("PostService", () => {
+  describe("getPostCatalog", () => {
+    it("should return post catalog", () => {
+      postService.getPostCatalog()
+      .then((res: Array<Post>) => {
+        expect(res).toBeInstanceOf(Array);
+      });
+    })
+  });
+  describe("uploadPost", () => {
+    it("should upload post without image", () => {
+      const start = jest.spyOn(mockQueryRunner, "startTransaction");
+      const commit = jest.spyOn(mockQueryRunner, "commitTransaction");
+      const rollback = jest.spyOn(mockQueryRunner, "rollbackTransaction");
+      const release = jest.spyOn(mockQueryRunner, "release");
+      return postService.uploadPost("rightId", { 
+        title: "title",  
+        content: "content",
+      }, [])
+      .then(() => {
+        expect(start).toBeCalled();
+        expect(commit).toBeCalled();
+        expect(rollback).toBeCalledTimes(0);
+        expect(release).toBeCalled();
+      });
     });
   });
+});
